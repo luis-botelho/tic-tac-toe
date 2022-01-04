5e359b5668314df60990467ef0493ca089c7cd53
luis-botelho <79022593+luis-botelho@users.noreply.github.com>
2022-01-03 22:20:07 -0300

    first commit

---
+++ tic-tac-toe/src/components/Board.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Board() {
+  return (
+    <div>
+      <div className="board-row">
+        <button className="square"></button>
+        <button className="square"></button>
+        <button className="square"></button>
+      </div>
+      <div className="board-row">
+        <button className="square"></button>
+        <button className="square"></button>
+        <button className="square"></button>
+      </div>
+      <div className="board-row">
+        <button className="square"></button>
+        <button className="square"></button>
+        <button className="square"></button>
+      </div>
+    </div>
+  );
+}
